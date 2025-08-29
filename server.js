58abfc6de3057ecefcae29531b3e7dbfc9f8d7f7
VishnuPujitha <153887300+VishnuPujitha@users.noreply.github.com>
2025-08-29 11:57:23 +0530

    Fix: added /bfhl POST route and updated package.json for Render

--- server.js
+++ server.js
@@ -1,43 +1,58 @@
-    app.post("/bfhl", (req, res) => {
-   const data = req.body.data;
+const express = require("express");
+const bodyParser = require("body-parser");
 
-   let oddNumbers = [];
-   let evenNumbers = [];
-   let alphabets = [];
-   let specialChars = [];
-   let sum = 0;
+const app = express();
+app.use(bodyParser.json());
 
-   data.forEach(item => {
-      if (!isNaN(item)) {
-         let num = parseInt(item);
-         sum += num;
-         if (num % 2 === 0) {
-            evenNumbers.push(item);
-         } else {
-            oddNumbers.push(item);
-         }
-      } else if (/^[a-zA-Z]+$/.test(item)) {
-         alphabets.push(item.toUpperCase());
+app.get("/", (req, res) => {
+  res.send("BFHL API is running. Use POST /bfhl");
+});
+
+app.post("/bfhl", (req, res) => {
+  const data = req.body.data;
+
+  let oddNumbers = [];
+  let evenNumbers = [];
+  let alphabets = [];
+  let specialChars = [];
+  let sum = 0;
+
+  data.forEach(item => {
+    if (!isNaN(item)) {
+      let num = parseInt(item);
+      sum += num;
+      if (num % 2 === 0) {
+        evenNumbers.push(item);
       } else {
-         specialChars.push(item);
+        oddNumbers.push(item);
       }
-   });
+    } else if (/^[a-zA-Z]+$/.test(item)) {
+      alphabets.push(item.toUpperCase());
+    } else {
+      specialChars.push(item);
+    }
+  });
 
-   let concatString = alphabets.join("");
-   concatString = concatString.split("").reverse()
-      .map((ch, i) => i % 2 === 0 ? ch.toUpperCase() : ch.toLowerCase())
-      .join("");
+  let concatString = alphabets.join("");
+  concatString = concatString.split("").reverse()
+    .map((ch, i) => (i % 2 === 0 ? ch.toUpperCase() : ch.toLowerCase()))
+    .join("");
 
-   res.json({
-      is_success: true,
-      user_id: "b_vishnu_pujitha_20052003",
-      email: "22bce9409@vitstudent.ac.in",
-      roll_number: "22BCE9409",
-      odd_numbers: oddNumbers,
-      even_numbers: evenNumbers,
-      alphabets: alphabets,
-      special_characters: specialChars,
-      sum: sum.toString(),
-      concat_string: concatString
-   });
+  res.json({
+    is_success: true,
+    user_id: "b_vishnu_poojitha_22092003",
+    email: "22bce9409@vitstudent.ac.in",
+    roll_number: "22BCE9409",
+    odd_numbers: oddNumbers,
+    even_numbers: evenNumbers,
+    alphabets: alphabets,
+    special_characters: specialChars,
+    sum: sum.toString(),
+    concat_string: concatString
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
